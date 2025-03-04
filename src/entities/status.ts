df112510596f3b7caa1c79624927489430c511b6
Jack Moran <jack@earth.co.nz>
2025-03-04 21:21:11 +1300

    Change Status to a union

--- src/entities/status.ts
+++ src/entities/status.ts
@@ -1,5 +1,9 @@
-export enum Status {
-  Unmarked = "Unmarked",
-  Cleared = "Cleared",
-  Pending = "Pending",
+export namespace Status {
+  export const Unmarked = "";
+  export const Cleared = "!";
+  export const Pending = "*";
 }
+export type Status =
+  | typeof Status.Unmarked
+  | typeof Status.Cleared
+  | typeof Status.Pending;
