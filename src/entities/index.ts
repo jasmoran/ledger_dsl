a9d7e3a9ea31dd24cd2fd386a01235e88f2176aa
Jack Moran <jack@earth.co.nz>
2025-03-04 21:16:25 +1300

    Export both DSL and entities

---
+++ src/entities/index.ts
@@ -0,0 +1,8 @@
+export { Amount } from "./amount";
+export { Comment } from "./comment";
+export { CostedAmount, CostType } from "./costed_amount";
+export { Journal } from "./journal";
+export { Posting } from "./posting";
+export { Status } from "./status";
+export { Transaction } from "./transaction";
+export { EMPTY_UNIT, Unit } from "./unit";
