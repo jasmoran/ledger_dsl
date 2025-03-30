02cc78c60bf6e2125b93aa2c2c7fb672cf3b9c3e
Jack Moran <jack@earth.co.nz>
2025-03-30 17:39:22 +1300

    Add journal sort function

--- src/entities/journal.ts
+++ src/entities/journal.ts
@@ -26,4 +26,28 @@ export class Journal {
   public toLedger(): string {
     return this.#entries.map((entry) => entry.toLedger(0)).join("\n\n");
   }
+
+  /**
+   * Sorts the journal entries by date.
+   * Places comments at the top of the journal.
+   * Transactions are sorted by date, then code, then note.
+   */
+  public sort(): void {
+    this.#entries.sort((a, b) => {
+      if (a instanceof Comment && b instanceof Comment) {
+        return 0;
+      } else if (a instanceof Comment) {
+        return -1;
+      } else if (b instanceof Comment) {
+        return 1;
+      }
+
+      return (
+        a.date.getTime() - b.date.getTime() ||
+        a.code?.localeCompare(b.code || "") ||
+        a.note?.localeCompare(b.note || "") ||
+        0
+      );
+    });
+  }
 }
